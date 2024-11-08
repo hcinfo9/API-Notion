f4eede47f1be3fef7fcc15fff5369ccb076a727d
hcinfo9 <hcinformatica9438@gmail.com>
2024-11-07 22:15:40 -0300

    Create a structure for folders and files API and Auth

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,4 @@
+import { JwtModule } from '@nestjs/jwt';
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
